ce9822afed57d57e0da22eee835f5961838d3053
이하택 <139848355+LeeHataeg@users.noreply.github.com>
2025-04-04 19:12:42 +0900

    [Add]_Scripts_GameManagers

---
+++ 3D Project/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // This script Manages Operation of Game
+    // Like Game Cycle( Gamestart,  etc )
+    private volatile static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            //TODO - Add details
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
