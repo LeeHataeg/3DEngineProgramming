b7c5f05a887c96bbfcb2280fdd809c6642e41b2f
이하택 <139848355+LeeHataeg@users.noreply.github.com>
2025-05-26 15:45:37 +0900

    [Test/#2] 테스트용 커밋

--- 3D Project/Assets/Scripts/Commons/Define.cs
+++ 3D Project/Assets/Scripts/Commons/Define.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public abstract class Define
 {
     #region Enum
+    public enum test
+    {
+        None = 0,
+        Job = 1
+    }
     #endregion
 
     #region Struct
     #endregion
 
     #region Class
+    
     #endregion
 }
