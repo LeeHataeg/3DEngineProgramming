cdc52ba10ca5314502b53d7be480d3b602f4fbcd
이하택 <139848355+LeeHataeg@users.noreply.github.com>
2025-06-06 19:36:09 +0900

    [Fix/#2] 삭제되었던 UI 복구

--- 3D Project/Assets/Scripts/PhysicsExperiment/UI/ExperimentUIController.cs
+++ 3D Project/Assets/Scripts/PhysicsExperiment/UI/ExperimentUIController.cs
@@ -13,13 +13,20 @@ public class ExperimentUIController : MonoBehaviour
         Time.timeScale = 0f;
     }
 
+    public void OnStopClick()
+    {
+        Time.timeScale = 0f;
+        GameManager.Instance.SceneChangeManager.SetPhysicsScene();
+    }
     public void OnReplayClick()
     {
         GameManager.Instance.SceneChangeManager.SetPhysicsScene();
+        Time.timeScale = 1f;
     }
 
     public void OnExitClick()
     {
         GameManager.Instance.SceneChangeManager.SetMainScene();
+        Time.timeScale = 1f;
     }
 }
